632d5491ca963ea843635792060707e17042bf5d
Akaing1 <33577017+Akaing1@users.noreply.github.com>
2019-11-03 14:13:21 -0500

    Update Run File.py

--- WeatherHere/Run File.py
+++ WeatherHere/Run File.py
@@ -1,19 +1,24 @@
 from WeatherDataCollection import WeatherCollection
 from GoogleMapsDirections import GoogleMapsDirections
+from Recommendations import Recommendations
+
 
 def run(start, end):
 
     getDir = GoogleMapsDirections((start, end), 'driving')
     startCord, endCord, googleDirs = getDir.getDirections()
-    print('Start:', start, startCord, '\nDestination:', end, endCord)
+    print('Start:', start, startCord, '\nDestination:', end, endCord, '\n')
 
     getWeatherStart = WeatherCollection((startCord['lat'], startCord['lng']))
     startData = getWeatherStart.get_weather()
-    print('Temperature in', start, ':', str(startData[0]) + 'F')
+    print('Current temperature in', start, ':', str(startData[0]) + 'F')
 
     getWeatherEnd = WeatherCollection((endCord['lat'], endCord['lng']))
     endData = getWeatherEnd.get_weather()
-    print('Temperature in', end, ':', str(endData[0]) + 'F')
+    print('Current temperature in', end, ':', str(endData[0]) + 'F')
+
+    getReco = Recommendations(startData, endData)
+    print('\nRoute Recommendations: ' + getReco.getRecommedation())
 
 
-run('sanfrancisco', 'Centreville')
+run('sandeigo', 'StateCollege')
